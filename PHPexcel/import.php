a30fb29c063ff96fe6f387fff3970b8a648d026c
JackeyEting <1193767343@qq.com>
2017-09-16 13:04:01 +0800

    Update import.php

--- PHPexcel/import.php
+++ PHPexcel/import.php
@@ -4,9 +4,25 @@ $filePath = "export_1.xls";
 $fileType = PHPExcel_IOFactory::identify($filePath); //文件名自动判断文件类型
 $objReader = PHPExcel_IOFactory::createReader($fileType);
 $objPHPExcel = $objReader->load($filePath);
-/*$sheet=$objPHPExcel->getSheet(1);//获取第一个索引表
-echo $sheet->getHighestRow();//取得总行数
-$highestColumm = $sheet->getHighestColumn(); // 取得总列数*/
+$objsheet=$objPHPExcel->getSheet(1);//获取第一个索引表
+countRow=$objsheet->getHighestRow();//取得总行数
+/*$highestColumm = $sheet->getHighestColumn(); // 取得总列数*/
+//设置单元的边框
+$styleThinBlackBorderOutline = array(
+       'borders' => array (
+             'outline' => array (
+                   'style' => PHPExcel_Style_Border::BORDER_THIN,   //设置border样式
+                   //'style' => PHPExcel_Style_Border::BORDER_THICK,  另一种样式
+                   'color' => array ('argb' => 'FF000000'),          //设置border颜色
+            ),
+      ),
+);
+for ($i=1; $i <= $countRow; $i++) { 
+	$objsheet->getStyle( 'A'.$i)->applyFromArray($styleThinBlackBorderOutline);
+	$objsheet->getStyle( 'B'.$i)->applyFromArray($styleThinBlackBorderOutline);
+	$objsheet->getStyle( 'C'.$i)->applyFromArray($styleThinBlackBorderOutline);
+}
+
 $savePath = './new_index.html'; //这里记得将文件名包含进去
 $objWriter = new PHPExcel_Writer_HTML($objPHPExcel); 
 $objWriter->setSheetIndex(1); //可以将括号中的0换成需要操作的sheet索引
