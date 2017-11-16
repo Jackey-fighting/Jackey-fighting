946f820822db833484aec148958c5f54dc9a2f22
JackeyEting <1193767343@qq.com>
2017-11-16 14:15:34 +0800

    工厂模式以及curl
    工厂模式以及curl

---
+++ curl.php
@@ -0,0 +1,24 @@
+﻿<?php
+/*发送 post 数据
+@param url $url 发送地址
+@param data $data 发送的json数据*/
+ function http_curl($url,$data){
+ 
+   	//1.初始化curl
+   	$ch = curl_init();
+  
+   	//2.设置curl的参数
+   	curl_setopt($ch, CURLOPT_URL, $url);
+   	curl_setopt($ch, CURLOPT_HEADER, 0);
+ 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+    curl_setopt($ch, CURLOPT_POST, 1);
+   	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+   	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+   	//3.采集
+   	$output = curl_exec($ch);
+   	//4.关闭
+	
+   	$output?var_dump($output):die(curl_error($ch));
+   	curl_close($ch);
+   }
---
+++ factoryClass.php
@@ -0,0 +1,35 @@
+﻿<?php
+//一个规定的父类
+abstract class Cal{
+	public abstract function calculation($num1,$num2);
+}
+
+class Add extends Cal{
+	public function calculation($num1,$num2){
+		return $num1+$num2;
+	}
+}
+
+class Reduce extends Cal{
+	public function calculation($num1,$num2){
+		return $num1-$num2;
+	}
+}
+
+//实例化的工厂类
+/*@param type $type 要创建的类型*/
+Class CalFactory{
+	public static function createObj($type){
+		switch($type){
+			case 'Add':
+				return new Add();
+				break;
+			case 'Reduce':
+				return new Reduce();
+				break;
+		}
+	}
+}
+
+$Obj=CalFactory::createObj('Reduce');
+echo $Obj->calculation(10,20);
