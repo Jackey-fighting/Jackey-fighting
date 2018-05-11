0e6ff8c234e0be767f3ea6a6b58e6a097d959081
JackeyEting <1193767343@qq.com>
2018-05-11 16:18:10 +0800

    引用
    引用objectid，然后通过objectid去找用户的具体详细地址

---
+++ MongoDB/引用.php
@@ -0,0 +1,24 @@
+<?php
+$mongo = new MongoClient();
+$db = $mongo->test;
+
+$person = $db->person;
+$address = $db->address;
+
+$myAddress = array(
+	'line 1' => '123 Main Street',
+	'line 2' => null,
+	'city'   => 'Springfield',
+	'state'  => 'Vermont',
+	'country'=> 'USA'
+);
+
+//保存address文档
+$address->insert($myAddress);
+
+//保存一个person关联到address
+$me = array('name'=>'Jackey', 'address'=>$myAddress['_id']);
+$person->insert($me);
+
+
+#引用的时候是将 address的地址放到 me的数组里面，采用的'address'(自定义)，对应上面刚插入的$myAddress数组
